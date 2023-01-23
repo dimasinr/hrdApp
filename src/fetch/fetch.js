d888e173d7435ac3d4bbe1ef8c4b95bf772b10cf
dimasinr <dimas.indra773@gmail.com>
2023-01-23 12:28:37 +0700

    change fetch

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://sdm.nawastratechnology.com"
-export const BASE_URL = "http://127.0.0.1:8000"
+export const BASE_URL = "https://sdm.nawastratechnology.com"
+// export const BASE_URL = "http://127.0.0.1:8000"
 export const USER_TOKEN = localStorage.getItem("user_token")
 export const API_KEY = "3628a84d67017e3260a2d04cdf487bb4f6f8b262"
 export const NAMES = localStorage.getItem("name")
