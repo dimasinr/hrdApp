02092a9cf44596327ad14d6bbbe9409909d8a3b9
dimasinr <dimas.indra773@gmail.com>
2023-01-24 19:13:39 +0700

    notif

--- src/oneSignal/oneSignal.js
+++ src/oneSignal/oneSignal.js
@@ -1,13 +1,24 @@
 import OneSignalReact from "react-onesignal";
 
 export default async function runOneSignal() {
-    await OneSignalReact.init({ appId: '9343c96f-aa0a-4af0-9f87-93d2c02b9ce0',
-     allowLocalhostAsSecureOrigin: true,
-     safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
+
+window.OneSignalReact = window.OneSignalReact || [];
+  OneSignalReact.push(function() {
+    OneSignalReact.init({
+      appId: "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
+      safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
       notifyButton: {
         enable: true,
       },
     });
-    OneSignalReact.showSlidedownPrompt();
+  });
+    // await OneSignalReact.init({ appId: '9343c96f-aa0a-4af0-9f87-93d2c02b9ce0',
+    //  allowLocalhostAsSecureOrigin: true,
+    //  safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
+    //   notifyButton: {
+    //     enable: true,
+    //   },
+    // });
+    // OneSignalReact.showSlidedownPrompt();
   }
   
