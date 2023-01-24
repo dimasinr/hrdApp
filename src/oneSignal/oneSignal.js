db65f9c25de411b5cf9e387819088cc04fffed7e
dimasinr <dimas.indra773@gmail.com>
2023-01-24 19:33:48 +0700

    notification

--- src/oneSignal/oneSignal.js
+++ src/oneSignal/oneSignal.js
@@ -2,16 +2,25 @@ import OneSignalReact from "react-onesignal";
 
 export default async function runOneSignal() {
 
-window.OneSignalReact = window.OneSignalReact || [];
-  OneSignalReact.push(function() {
-    OneSignalReact.init({
-      appId: "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
-      safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
+await OneSignalReact.init({
+  appId: "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
+  safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
       notifyButton: {
         enable: true,
       },
-    });
-  });
+ });
+
+
+// window.OneSignalReact = window.OneSignalReact || [];
+//   OneSignalReact.push(function() {
+//     OneSignalReact.init({
+//       appId: "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
+//       safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
+//       notifyButton: {
+//         enable: true,
+//       },
+//     });
+//   });
     // await OneSignalReact.init({ appId: '9343c96f-aa0a-4af0-9f87-93d2c02b9ce0',
     //  allowLocalhostAsSecureOrigin: true,
     //  safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
