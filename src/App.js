f57c1445f10bdd23bc07ef05de96d1124b3e4b5d
dimasinr <dimas.indra773@gmail.com>
2023-01-24 19:49:34 +0700

    push notification

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import { getApp } from './utils/helpers';
+import runOneSignal from './oneSignal/oneSignal';
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    runOneSignal();
+  })
   const CurrentApp = getApp()
   return (
     <div className="App">
