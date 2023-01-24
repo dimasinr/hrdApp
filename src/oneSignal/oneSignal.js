9b43613af78bf85a44af031684da4f0ccc3c432f
dimasinr <dimas.indra773@gmail.com>
2023-01-24 19:08:07 +0700

    push notification

--- src/oneSignal/oneSignal.js
+++ src/oneSignal/oneSignal.js
@@ -1,7 +1,13 @@
 import OneSignalReact from "react-onesignal";
 
 export default async function runOneSignal() {
-    await OneSignalReact.init({ appId: '41c395bc-3480-4b7e-b339-75c9265db19a', allowLocalhostAsSecureOrigin: true});
+    await OneSignalReact.init({ appId: '9343c96f-aa0a-4af0-9f87-93d2c02b9ce0',
+     allowLocalhostAsSecureOrigin: true,
+     safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
+      notifyButton: {
+        enable: true,
+      },
+    });
     OneSignalReact.showSlidedownPrompt();
   }
   
