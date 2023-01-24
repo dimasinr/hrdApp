84c20efa3158eae2ccf695f603e8b48df90c5425
dimasinr <dimas.indra773@gmail.com>
2023-01-24 19:43:31 +0700

    push notification

--- src/oneSignal/oneSignal.js
+++ src/oneSignal/oneSignal.js
@@ -11,16 +11,34 @@ export default async function runOneSignal() {
 //  });
 
 
-window.OneSignalReact = window.OneSignalReact || [];
-  OneSignalReact.push(function() {
-    OneSignalReact.init({
-      appId: "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
-      safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
-      notifyButton: {
-        enable: true,
-      },
+// window.OneSignalReact = window.OneSignalReact || [];
+//   OneSignalReact.push(function() {
+//     OneSignalReact.init({
+//       appId: "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
+//       safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
+//       notifyButton: {
+//         enable: true,
+//       },
+//     });
+//   });
+
+
+  var OneSignal = window.OneSignalReact || [];
+OneSignal.push(["init", {
+    appId           :   "9343c96f-aa0a-4af0-9f87-93d2c02b9ce0",
+    autoRegister    :   true, /* Set to true to automatically prompt visitors */
+    subdomainName   :   "https://sdm-app.netlify.app/",
+    notifyButton    :   {
+        enable: true /* Set to false to hide */
+    }
+}]);
+
+OneSignal.push(function() {
+    OneSignal.once("init", function(event) {
+        alert("testt");
     });
-  });
+ });
+
     // await OneSignalReact.init({ appId: '9343c96f-aa0a-4af0-9f87-93d2c02b9ce0',
     //  allowLocalhostAsSecureOrigin: true,
     //  safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
