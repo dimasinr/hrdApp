863b0098fe22e068a88c7168b5b6e2c4bdf7e92e
dimasinr <dimas.indra773@gmail.com>
2023-01-24 20:06:17 +0700

    push

--- src/oneSignal/oneSignal.js
+++ src/oneSignal/oneSignal.js
@@ -41,6 +41,7 @@ OneSignalReact.getSubscription()
     });
 //  });
 
+
     // await OneSignalReact.init({ appId: '9343c96f-aa0a-4af0-9f87-93d2c02b9ce0',
     //  allowLocalhostAsSecureOrigin: true,
     //  safari_web_id: "web.onesignal.auto.6187ce57-f346-4a86-93e4-7d70d494c000",
