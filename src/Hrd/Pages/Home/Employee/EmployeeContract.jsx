f410329c54ca454c0b87c5c8dc027039dad117ce
dimasinr <dimas.indra773@gmail.com>
2023-04-06 15:43:25 +0700

    masih typo

--- src/Hrd/Pages/Home/Employee/EmployeeContract.jsx
+++ src/Hrd/Pages/Home/Employee/EmployeeContract.jsx
@@ -52,7 +52,7 @@ function EmployeeContract() {
     const [employee, setEmployee] = React.useState([])
 
     const getListPengajuan = () => {
-        axios.get(`${BASE_URL}/users/employee/contract/?active_user=true`,{
+        axios.get(`${BASE_URL}/users/employee/contract/?active_user=True`,{
           headers: {
             "Authorization" : 'Token ' + USER_TOKEN
           }
